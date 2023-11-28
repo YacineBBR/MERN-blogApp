26cc7490b0e76cf30570cfeef516752b61954b50
YacineBBR <94230383+YacineBBR@users.noreply.github.com>
2023-11-28 16:51:51 +0100

    creating layouts with ejs

--- server/app.js
+++ server/app.js
@@ -4,11 +4,11 @@ const expressLayout = require("express-ejs-layouts");
 const req = require("express/lib/request");
 const app = express();
 const PORT = 5000 || process.env.PORT;
-app.use(express.static('public'))
+app.use(express.static("public"));
 app.use(expressLayout);
-app.set("layout", "./layout/main");
+app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
-app.use("/main", require('./server/routes/main'))
+app.use("/", require("./server/routes/main"));
 app.listen(PORT, () => {
   console.log(`server is running on ${PORT}`);
 });
--- server/server/routes/main.js
+++ server/server/routes/main.js
@@ -1,9 +1,17 @@
 const express = require("express");
-const router = express.Router()
+const router = express.Router();
 
-
+// adding routes & setting up the pages to be rendered
 
 router.get("", (req, res) => {
-    res.send("hello world");
-  });
+  const locals = {
+    title: "BBR's wiki",
+    description: "simple blog app created with node js, express & mongo db",
+  };
+  res.render("index", { locals });
+});
+router.get("/about", (req, res) => {
+  res.render("about");
+});
+
 module.exports = router;
